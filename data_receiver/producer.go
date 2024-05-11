742c117c52b76355c25d84ba17174c14def133e0
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-11 13:41:39 +0200

    put data producer into separate file, cleaning code up + fixed script bug

---
+++ data_receiver/producer.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/lucaliebenberg/tolling/types"
+)
+
+type DataProducer interface {
+	ProduceData(types.OBUData) error
+}
+
+type KafkaProducer struct {
+	producer *kafka.Producer
+}
+
+func NewKafkaProducer() (*KafkaProducer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+	// Start another goroutine to check if we have delivered the data
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return &KafkaProducer{
+		producer: p,
+	}, nil
+}
+
+func (p *KafkaProducer) ProduceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	return p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &kafkaTopic,        // Kafka topic to which the message will be sent
+			Partition: kafka.PartitionAny, // Partition of the topic where the message will be sent
+		},
+		Value: b, // Serialized data to be sent as the message value
+	}, nil)
+}
