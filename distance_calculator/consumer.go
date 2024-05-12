80308b236cbbd2c70ead176ebcfcfa1fe9db862c
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-12 12:06:02 +0200

    added consumer and is correctly receiving obudata from data producer

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -1,4 +1,47 @@
 package main
 
-type DataConsumer interface {
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+// This can also be called a KafkaTransporter
+type KafkaConsumer struct {
+	consumer  *kafka.Consumer
+	isRunning bool
+}
+
+func NewKafkaConsumer(topic string) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, err
+	}
+	c.SubscribeTopics([]string{topic}, nil)
+	return &KafkaConsumer{
+		consumer: c,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+
+	for c.isRunning {
+		msg, err := c.consumer.ReadMessage(-1) // could set to every second -> (time.Second)
+		if err != nil {
+			logrus.Errorf("kafka consume error %s", err)
+			continue
+		}
+		fmt.Println(msg)
+	}
 }
