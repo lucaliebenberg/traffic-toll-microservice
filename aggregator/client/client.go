2f41a1e0a87727dbf117e2e104ffde797010bf30
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-25 21:54:06 +0200

    added client for service metrics

---
+++ aggregator/client/client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/lucaliebenberg/tolling/types"
+)
+
+type Client struct {
+	Endpoint string
+}
+
+func NewClient(endpoint string) *Client {
+	return &Client{
+		Endpoint: endpoint,
+	}
+}
+
+func (c *Client) AggregateInvoice(distance types.Distance) error {
+	httpc := http.DefaultClient{}
+	b, err := json.Marshal(distance)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", c.Endpoint, bytes.NewReader(b))
+}
