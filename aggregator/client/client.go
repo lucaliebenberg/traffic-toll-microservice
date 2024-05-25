cfc938d24d01b6e4b1d2205fd016f2a78359b655
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-25 22:49:24 +0200

    added client

--- aggregator/client/client.go
+++ aggregator/client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/lucaliebenberg/tolling/types"
@@ -19,10 +20,17 @@ func NewClient(endpoint string) *Client {
 }
 
 func (c *Client) AggregateInvoice(distance types.Distance) error {
-	httpc := http.DefaultClient{}
 	b, err := json.Marshal(distance)
 	if err != nil {
 		return err
 	}
 	req, err := http.NewRequest("POST", c.Endpoint, bytes.NewReader(b))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("teh service respinded with non 200 status code %d", resp.StatusCode)
+	}
+	return nil
 }
